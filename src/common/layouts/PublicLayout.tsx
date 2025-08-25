19cf665bfa059f4761578997ac78b2d9616ee865
Fred Muju <fredmuju@gmail.com>
2025-08-25 11:36:05 +0300

    refactor: Clean up commented code and improve readability in PublicLayout component

--- src/common/layouts/PublicLayout.tsx
+++ src/common/layouts/PublicLayout.tsx
@@ -7,49 +7,30 @@ import { PublicFooter } from "../navs/dashboard/footer/PublicFooter";
 import Header from "../navs/dashboard/header/Header";
 
 export default function PublicLayout() {
-  // const [opened, { toggle }] = useDisclosure();
   const [opened] = useDisclosure();
-  // const { updateUserDevice } = useAuthServices();
   const [_userLocation, setUserLocation] = useState<{ latitude: number; longitude: number }>();
   async function requestPermission() {
-    //requesting permission using Notification API
     const permission = await Notification.requestPermission();
 
     if (permission === "granted") {
-      // const token = await getToken(messaging, {
-      //   vapidKey: Env.APP_VAPID_KEY,
-      // });
-      // await updateUserDevice(token);
-
-      //We can send token to server
-      // console.log("Token generated : ", token);
+      console.log("Notification permission granted.");
     } else if (permission === "denied") {
-      //notifications are blocked
       alert("You denied for the notification");
     }
   }
 
-  // const variable array to save the users location
-
-  // define the function that finds the users geolocation
   const getUserLocation = () => {
-    // if geolocation is supported by the users browser
     if (navigator.geolocation) {
-      // get the current users location
       navigator.geolocation.getCurrentPosition(
         (position) => {
-          // save the geolocation coordinates in two variables
           const { latitude, longitude } = position.coords;
-          // update the value of userlocation variable
           setUserLocation({ latitude, longitude });
         },
-        // if there was an error getting the users location
         (error) => {
           console.error('Error getting user location:', error);
         }
       );
     }
-    // if geolocation is not supported by the users browser
     else {
       console.error('Geolocation is not supported by this browser.');
     }
